ecb1ddc420327daafa9fbcbb64eba19d629a313a
hugnt <thanhhungst314@gmail.com>
2023-11-07 21:07:57 +0700

    push admin UI

---
+++ SmartHRM.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHRM.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
