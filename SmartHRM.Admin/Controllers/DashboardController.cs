bf96ae2c2cec4fe7cb36d92cf750471f0f9e1f57
Hoang1973 <126128764+Hoang1973@users.noreply.github.com>
2023-11-21 21:25:51 +0700

    Hoang Update controller Dashboard

---
+++ SmartHRM.Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartHRM.Admin.Controllers
+{
+	public class DashboardController : Controller
+	{
+        [Route("/Hoang/Dashboard")]
+        public IActionResult HoangIndex()
+        {
+            return View();
+        }
+
+    }
+}
+
